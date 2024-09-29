34b3cff89096df6af2a840ec8175de546188b520
Nicholas Liu <liunicholas6@gmail.com>
2024-09-29 14:22:52 -0400

    Fixed ray jitter

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -149,14 +149,12 @@ __global__ void generateRayFromCamera(Camera cam, int iter, int traceDepth, Path
         segment.radiance = glm::vec3();
 
         auto rng = makeSeededRandomEngine(iter, index, -1);
-        thrust::uniform_real_distribution<float> u01(0, 1);
-        x += u01(rng);
-        y += u01(rng);        
+        thrust::uniform_real_distribution<float> u01(0, 1); 
 
         // TODO: implement antialiasing by jittering the ray
         segment.ray.direction = glm::normalize(cam.view
-            - cam.right * cam.pixelLength.x * ((float)x - (float)cam.resolution.x * 0.5f)
-            - cam.up * cam.pixelLength.y * ((float)y - (float)cam.resolution.y * 0.5f)
+            - cam.right * cam.pixelLength.x * (static_cast<float>(x) + u01(rng) - static_cast<float>(cam.resolution.x) * 0.5f)
+            - cam.up * cam.pixelLength.y * (static_cast<float>(y) + u01(rng) - static_cast<float>(cam.resolution.y) * 0.5f)
         );
 
         segment.pixelIndex = index;
