4d7d34ddf818b7fd42cf7248e0e6ac4acd40143d
hazard-10 <bradleyx@umich.edu>
2024-10-03 11:11:57 -0700

    add anti-aliasing

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -176,9 +176,15 @@ __global__ void generateRayFromCamera(Camera cam, int iter, int traceDepth, Path
         segment.color = glm::vec3(1.0f, 1.0f, 1.0f);
 
         // TODO: implement antialiasing by jittering the ray
+        thrust::default_random_engine rng = makeSeededRandomEngine(iter, index, 0);
+        thrust::uniform_real_distribution<float> u01(0, 1);
+        // Generate  offsets in local pixel
+        float dx = u01(rng) - 0.5f;
+        float dy = u01(rng) - 0.5f;
+        
         segment.ray.direction = glm::normalize(cam.view
-            - cam.right * cam.pixelLength.x * ((float)x - (float)cam.resolution.x * 0.5f)
-            - cam.up * cam.pixelLength.y * ((float)y - (float)cam.resolution.y * 0.5f)
+            - cam.right * cam.pixelLength.x * ((float)x - (float)cam.resolution.x * 0.5f + dx)
+            - cam.up * cam.pixelLength.y * ((float)y - (float)cam.resolution.y * 0.5f + dy)
         );
 
         segment.pixelIndex = index;
