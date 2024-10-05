6b2395df7d02c5ed2f8154805be8bc9e070780d0
Adithya Rajeev <adithyar262@gmail.com>
2024-10-04 22:39:28 -0400

    [Part 2] Implemented Direct Light Sampling
    Handling only Cube and spherical light sources.

--- src/interactions.cu
+++ src/interactions.cu
@@ -40,12 +40,59 @@ __host__ __device__ glm::vec3 calculateRandomDirectionInHemisphere(
         + sin(around) * over * perpendicularDirection2;
 }
 
+__host__ __device__  glm::vec3 samplePointOnLight(const Geom& geom, thrust::default_random_engine& rng)
+{
+    thrust::uniform_real_distribution<float> u01(0, 1);
+
+    if (geom.type == SPHERE)
+    {
+        float theta = 2 * PI * u01(rng);
+        float phi = acos(2 * u01(rng) - 1);
+        float r = geom.scale.x; // Assuming uniform scale for sphere
+
+        glm::vec3 localPoint(
+            r * sin(phi) * cos(theta),
+            r * sin(phi) * sin(theta),
+            r * cos(phi)
+        );
+
+        return geom.translation + glm::vec3(geom.transform * glm::vec4(localPoint, 1.0f));
+    }
+    else if (geom.type == CUBE)
+    {
+        // Randomly choose one of the 6 faces
+        int face = int(u01(rng) * 6);
+        float u = u01(rng);
+        float v = u01(rng);
+
+        glm::vec3 localPoint;
+        switch (face)
+        {
+        case 0: localPoint = glm::vec3(-0.5f, u - 0.5f, v - 0.5f); break; // Left
+        case 1: localPoint = glm::vec3(0.5f, u - 0.5f, v - 0.5f); break; // Right
+        case 2: localPoint = glm::vec3(u - 0.5f, -0.5f, v - 0.5f); break; // Bottom
+        case 3: localPoint = glm::vec3(u - 0.5f, 0.5f, v - 0.5f); break; // Top
+        case 4: localPoint = glm::vec3(u - 0.5f, v - 0.5f, -0.5f); break; // Back
+        case 5: localPoint = glm::vec3(u - 0.5f, v - 0.5f, 0.5f); break; // Front
+        }
+
+        return geom.translation + glm::vec3(geom.transform * glm::vec4(localPoint, 1.0f));
+    }
+
+    // Default case (shouldn't happen)
+    return geom.translation;
+}
+
+
 __host__ __device__ void scatterRay(
     PathSegment& pathSegment,
     glm::vec3 intersect,
     glm::vec3 normal,
     const Material& m,
-    thrust::default_random_engine& rng)
+    thrust::default_random_engine& rng,
+    Geom* geoms,
+    int geoms_size,
+    Material* materials)
 {
     if (pathSegment.remainingBounces == 0)
         return;
@@ -57,6 +104,8 @@ __host__ __device__ void scatterRay(
     float rand = u01(rng);
 
     glm::vec3 newDirection;
+    glm::vec3 indirectLight(0.0f);
+    glm::vec3 directLight(0.0f);
 
     if (m.plasticSpecular > 0.0f)
     {
@@ -124,6 +173,57 @@ __host__ __device__ void scatterRay(
         pathSegment.color *= m.color;
     }
 
+    indirectLight = pathSegment.color;
+
+#if DIRECT_LIGHTING
+    // Compute direct lighting
+    for (int i = 0; i < geoms_size; ++i)
+    {
+        if (materials[geoms[i].materialid].emittance > 0.0f)
+        {
+            glm::vec3 lightPoint = samplePointOnLight(geoms[i], rng);
+            glm::vec3 lightDir = glm::normalize(lightPoint - intersect);
+            float lightDistance = glm::length(lightPoint - intersect);
+
+            Ray shadowRay;
+            shadowRay.origin = intersect + normal * 0.001f; // Offset to avoid self-intersection
+            shadowRay.direction = lightDir;
+            bool occluded = false;
+
+            for (int j = 0; j < geoms_size; ++j)
+            {
+                if (j != i) // Don't check intersection with the light itself
+                {
+                    float t;
+                    glm::vec3 tmpIntersect, tmpNormal;
+                    bool tmpOutside;
+                    if (geoms[j].type == CUBE)
+                        t = boxIntersectionTest(geoms[j], shadowRay, tmpIntersect, tmpNormal, tmpOutside);
+                    else if (geoms[j].type == SPHERE)
+                        t = sphereIntersectionTest(geoms[j], shadowRay, tmpIntersect, tmpNormal, tmpOutside);
+
+                    if (t > 0 && t < lightDistance)
+                    {
+                        occluded = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!occluded)
+            {
+                float cosTheta = glm::max(glm::dot(normal, lightDir), 0.0f);
+                glm::vec3 lightColor = materials[geoms[i].materialid].color;
+                float lightIntensity = materials[geoms[i].materialid].emittance;
+
+                glm::vec3 brdf = m.color / PI;
+
+                directLight += (brdf * lightColor * lightIntensity * cosTheta) / (lightDistance * lightDistance);
+            }
+        }
+    }
+#endif
+    pathSegment.color = indirectLight + directLight;
     pathSegment.ray.origin = intersect + newDirection * 0.001f;
     pathSegment.ray.direction = glm::normalize(newDirection);
     pathSegment.remainingBounces--;
